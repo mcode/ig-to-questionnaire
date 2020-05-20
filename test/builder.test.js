3cf14dd3743acf63046845eb753bc6cf1f1f5bc8
Matthew Gramigna <mgramigna@mitre.org>
2020-05-20 14:33:55 -0400

    Added QuestionnaireBuilder class skeleton and basic unit test

---
+++ test/builder.test.js
@@ -0,0 +1,12 @@
+const { Fhir } = require('fhir');
+const QuestionnaireBuilder = require('../src/builder');
+
+const fhir = new Fhir();
+
+describe('test questionnaire building', () => {
+  test('no items should be valid questionnaire', () => {
+    const builder = new QuestionnaireBuilder();
+    const { valid } = fhir.validate(builder.resource);
+    expect(valid).toBe(true);
+  });
+});
