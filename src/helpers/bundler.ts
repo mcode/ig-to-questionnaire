55231e4be1af8c013bfd51b0fef7d2671c97fafc
Matthew Gramigna <mgramigna@mitre.org>
2020-06-17 14:15:01 -0400

    Fix types to allow resource type/request in txn bundle

--- src/helpers/bundler.ts
+++ src/helpers/bundler.ts
@@ -1,9 +1,15 @@
 import { R4 } from '@ahryman40k/ts-fhir-types';
 
-export function bundlify(resources: Array<R4.IResource>): R4.IBundle {
+export function bundlify(resources: Array<R4.IResourceList>): R4.IBundle {
   return {
     resourceType: 'Bundle',
     type: R4.BundleTypeKind._transaction,
-    entry: resources
+    entry: resources.map(r => ({
+      resource: r,
+      request: {
+        method: R4.Bundle_RequestMethodKind._put,
+        url: `${r.resourceType}/${r.id}`
+      }
+    }))
   };
 }
