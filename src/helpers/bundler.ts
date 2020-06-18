689a2dac936be0b12dd3341dc138a070ca2f522a
Matthew Gramigna <mgramigna@mitre.org>
2020-06-18 09:43:25 -0400

    Use collection bundle instead

--- src/helpers/bundler.ts
+++ src/helpers/bundler.ts
@@ -1,15 +1,11 @@
 import { R4 } from '@ahryman40k/ts-fhir-types';
 
-export function bundlify(resources: Array<R4.IResourceList>): R4.IBundle {
+export function bundlify(resources: R4.IResourceList[]): R4.IBundle {
   return {
     resourceType: 'Bundle',
-    type: R4.BundleTypeKind._transaction,
+    type: R4.BundleTypeKind._collection,
     entry: resources.map(r => ({
-      resource: r,
-      request: {
-        method: R4.Bundle_RequestMethodKind._put,
-        url: `${r.resourceType}/${r.id}`
-      }
+      resource: r
     }))
   };
 }
